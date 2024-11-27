10a459cf89c194f8a77ddde046f5e692fd74bf75
hlenarocha <helenavitoria1200@gmail.com>
2024-11-27 19:32:05 -0300

    Teste do endpoint POST /test-post

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TesteController;
+use App\Models\Post;
+use Illuminate\Http\Request;
 
-Route::get('/hello', function () {
-    return response()->json(['message' => 'Oi do Laravel!']);
+// Rotas com o middleware de CORS aplicado
+Route::middleware('cors')->group(function () {
+
+    Route::get('/hello', function () {
+        return response()->json(['message' => 'Oi do Laravel!']);
+    });
+
+    Route::get('/teste', [TesteController::class, 'index'])->name('teste.index');
+
+    Route::post('/test-post', function (Request $request) {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $post = Post::create([
+            'name' => $validatedData['name'],
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'data' => $post,
+        ]);
+    });
+
 });
-
-Route::get('/teste', [App\Http\Controllers\TesteController::class, 'index'])->name('teste.index');
